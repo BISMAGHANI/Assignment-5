f881b8a7166284624daec67c41286e74031439ac
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-03-13 23:50:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var famous_person = "Nelson Mandela";
+var message = "Do not Judge me by my Success, Judge me by how many times I fell down and got backup again";
+console.log("".concat(famous_person, " once said, ").concat(message));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let famous_person :string = "Nelson Mandela";
+let message : string = "Do not Judge me by my Success, Judge me by how many times I fell down and got backup again";
+
+console.log(`${famous_person} once said, ${message}`);
